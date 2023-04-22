cce6eb090e9975eda8542af00b328caa59f349cd
Manav <manvashishth19@gmail.com>
2023-04-22 19:51:42 +0530

    errors resolved3

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
-import Post from '../mongodb/models/post.js';
+import Post from '../mongoDB/models/post.js';
 
 dotenv.config();
 
