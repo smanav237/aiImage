aea4d83e0826a1ac7d56b29f57c98b138e9c1689
Manav <manvashishth19@gmail.com>
2023-04-27 23:07:36 +0530

    frontend resolved

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,7 @@
 import download from './download.png';
 import logo from './logo.jpg';
 import preview from './preview.jpg';
-import bgImage32 from './bgImage33.jpg';
+import bgImage32 from './bgImage32.jpeg';
 import loaders from './x.gif'
 
 export {
